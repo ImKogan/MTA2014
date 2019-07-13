31a0658681b9db044bc68f8d619abfe5aab7ad17
Isroel Kogan <isroelkogan@yahoo.com>
2019-07-12 20:54:26 -0400

    minor bug update

--- MTA/setup/load-static-db.sh
+++ MTA/setup/load-static-db.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 pushd ../django_app/mta
 pwd
 
-./manage.py populate_db /vagrant/MTA/data/static/gtfs_static/routes.txt routes_to_sql
+./manage.py populate_db ../../data/static/gtfs_static/routes.txt routes_to_sql
 ./manage.py populate_db ../../data/static/nybb_19a/nybb.shp load_boros
 ./manage.py populate_db ../../data/shapefiles/stops.shp load_stops
 ./manage.py populate_db ../../data/shapefiles/shapes.shp load_shapes
