88740c2081cd8f711f12ae59f3152487fa618970
Isroel Kogan <isroelkogan@yahoo.com>
2019-10-06 18:52:06 -0400

    refactoring settings import

--- MTA/django_app/mta/mta2014/create_index.py
+++ MTA/django_app/mta/mta2014/create_index.py
@@ -6,23 +6,24 @@ create unique index in mta2014_trip table on (start_date, trip_id, stop_id)
 '''
 
 import os
+import argparse
 from configparser import RawConfigParser
 import psycopg2
 
-BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-config = RawConfigParser()
-config.read(os.path.join(BASE, 'settings.ini'))
-DATABASE = {'HOST': config.get('database', 'DATABASE_HOST'),
-            'PORT': config.get('database', 'DATABASE_PORT'),
-            'NAME': config.get('database', 'DATABASE_NAME'),
-            'USER': config.get('database', 'DATABASE_USER'),
-            'PASSWORD': config.get('database', 'DATABASE_PASSWORD'),
-           }
+from db_conn import db_connection
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--settings', default='settings.ini', help="name of settings .ini file")
+args = parser.parse_args()
+
+BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sql = os.path.join(BASE, 'mta2014', 'sql', 'create_index.sql')
 with open(sql) as f:
     SQL = f.read()
 
+settings = path['settings']
+DATABASE = db_connection(settings)
 conn = psycopg2.connect(
     "host={HOST} port={PORT} dbname={NAME} user={USER} password={PASSWORD}".format(**DATABASE))
 cur = conn.cursor()
